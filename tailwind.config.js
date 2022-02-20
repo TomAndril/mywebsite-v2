e1aeda74ee71d426a5c19274ff40cd0feac7b9a1
Tomas Nasjleti <acatomas@gmail.com>
2022-02-21 00:57:13 +0100

    tailwind config fix

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,7 @@ module.exports = {
     "./pages/**/*.{js,ts,jsx,tsx}",
     "./components/**/*.{js,ts,jsx,tsx}",
   ],
-  purge: ["./pages/**/*.{js,ts,jsx,tsx}", "./components/**/*.{js,ts,jsx,tsx}"],
+  purge: ["./pages/**/*.{js,ts,jsx,tsx}", "./components/**/*.{js,ts,jsx,tsx}", "./sections/**/*.{js,ts,jsx,tsx}"],
   theme: {
     typography: (theme) => ({}),
     extend: {},

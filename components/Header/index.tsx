bd8efafde273a8d8f4ef2451db3d9862715986c8
Tomas Nasjleti <acatomas@gmail.com>
2022-02-25 19:09:18 +0100

    removed border on header

--- components/Header/index.tsx
+++ components/Header/index.tsx
@@ -3,7 +3,7 @@ import PageLinks from "./PageLinks"
 
 const Header: React.FC = () => {
   return (
-    <header className="sticky top-0 z-50 flex items-center h-20 transition-all duration-300 bg-white border-b dark:bg-slate-900 dark:border-b-slate-700">
+    <header className="sticky top-0 z-50 flex items-center h-20 transition-all duration-300 bg-white dark:bg-slate-900">
       <nav className="flex flex-wrap justify-between w-full">
         <PageLinks />
         <SocialLinks />

02d31e30886eaa54ab2bec20726c4982e1da0459
Tomas Nasjleti <acatomas@gmail.com>
2022-02-26 15:51:35 +0100

    fixed fonts preconnect

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -17,13 +17,6 @@ class Doc extends Document {
     return (
       <Html lang="en">
         <Head>
-          <link rel="preload" href="https://fonts.googleapis.com" as="font" />
-          <link
-            rel="preload"
-            as="font"
-            href="https://fonts.gstatic.com"
-            crossOrigin="true"
-          />
           <link rel="preconnect" href="https://fonts.googleapis.com" />
           <link
             rel="preconnect"
@@ -31,7 +24,7 @@ class Doc extends Document {
             crossOrigin="true"
           />
           <link
-            href="https://fonts.googleapis.com/css2?family=Rubik:ital,wght@0,200;0,300;0,400;0,500;0,600;0,700;0,800;1,200;1,300;1,400;1,500;1,600;1,700;1,800&display=swap"
+            href="https://fonts.googleapis.com/css2?family=Rubik:ital,wght@0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap"
             rel="stylesheet"
           />
 

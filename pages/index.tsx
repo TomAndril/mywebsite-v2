765fdab2f7fe8ad0968398ee2bb9175c52791c43
Tomas Nasjleti <acatomas@gmail.com>
2022-03-06 19:50:02 +0100

    fixed typo

--- pages/index.tsx
+++ pages/index.tsx
@@ -11,7 +11,7 @@ const Home: NextPage = () => {
   return (
     <BaseLayout
       title="Tomas Nasjleti - Web Developer"
-      description="Tomas Nasjleti Personal Website. You can find information about me, the technlogies I use and some of the projects I have worked on."
+      description="Tomas Nasjleti Personal Website. You can find information about me, the technologies I use and some of the projects I have worked on."
     >
       <Hero />
       <MySkills />

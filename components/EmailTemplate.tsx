e2c3fd437cc43ca1fe9c6e26b200ebc80d05b68e
Tomas Nasjleti <acatomas@gmail.com>
2023-08-12 17:00:24 +0200

    slight template modification

--- components/EmailTemplate.tsx
+++ components/EmailTemplate.tsx
@@ -7,9 +7,9 @@ interface Props {
 export default function EmailTemplate({ email, message, name }: Props) {
   return (
     <div>
-      <h1>Hi, {name}</h1>
+      <h1>Hi! {name} sent you a message</h1>
       <p>{message}</p>
-      <p>From: {email}</p>
+      <p>Reply to: {email}</p>
     </div>
   );
 }

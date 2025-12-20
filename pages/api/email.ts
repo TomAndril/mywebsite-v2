cb10df258c55d3c23dd3da580c9fa463cc895e50
Tomas Nasjleti <acatomas@gmail.com>
2025-12-20 17:31:13 +0100

    fix

--- pages/api/email.ts
+++ pages/api/email.ts
@@ -2,6 +2,7 @@ import EmailTemplate from "@components/EmailTemplate";
 import type { NextApiRequest, NextApiResponse } from "next";
 import { Resend } from "resend";
 import { validateTurnstileToken } from "next-turnstile";
+import { v4 } from "uuid";
 
 type IReq = {
   name: string;
@@ -19,24 +20,26 @@ export default async function handler(
   if (req.method === "POST") {
     const { email, message, name, turnstileToken }: IReq = req.body;
 
+    const validationResponse = await validateTurnstileToken({
+      token: turnstileToken,
+      secretKey: process.env.NEXT_TURNSTILE_SECRET_KEY!,
+      idempotencyKey: v4(),
+    });
+
+    if (!validationResponse.success) {
+      return res
+        .status(400)
+        .json({ success: false, error: "Invalid captcha verification" });
+    }
+
     try {
-      const { success } = await validateTurnstileToken({
-        token: turnstileToken,
-        secretKey: process.env.NEXT_TURNSTILE_SECRET_KEY!,
-      });
-
-      if (!success) {
-        return res
-          .status(400)
-          .json({ success: false, error: "Invalid captcha verification" });
-      }
-
       const data = await resend.emails.send({
         from: process.env.RESEND_SENDER as string,
         to: process.env.RESEND_RECEIVER as string,
         subject: `New message from ${name}`,
         react: EmailTemplate({ email, message, name }),
       });
+
       if (data) {
         res.status(200).json({ success: true });
       }

9a047c91e671580c55b501a755eee6cf63edee2d
Tomas Nasjleti <acatomas@gmail.com>
2025-12-20 18:24:50 +0100

    fix theme icon

--- components/Header/SocialLinks.tsx
+++ components/Header/SocialLinks.tsx
@@ -1,16 +1,16 @@
-import { useTheme } from "next-themes"
+import { useTheme } from "next-themes";
 import {
   Github,
   Icon,
   Linkedin,
   MoonFill,
   SunFill,
-} from "react-bootstrap-icons"
+} from "react-bootstrap-icons";
 
 interface ISocialLink {
-  Component: Icon
-  url: string
-  label: string
+  Component: Icon;
+  url: string;
+  label: string;
 }
 
 const socialLinks: ISocialLink[] = [
@@ -24,15 +24,15 @@ const socialLinks: ISocialLink[] = [
     url: "https://www.linkedin.com/in/tominasweb/",
     label: "Linkedin Profile",
   },
-]
+];
 
 const iconProps = {
   width: 16,
   height: 16,
-}
+};
 
 const SocialLinks: React.FC = () => {
-  const { theme, setTheme } = useTheme()
+  const { theme, setTheme } = useTheme();
 
   return (
     <ul className="flex items-center justify-center text-black dark:text-white">
@@ -54,14 +54,14 @@ const SocialLinks: React.FC = () => {
         className="p-2 rounded cursor-pointer"
       >
         <div className="dark:hidden">
-          <MoonFill {...iconProps} />
+          <MoonFill {...iconProps} color="black" />
         </div>
         <div className="hidden dark:block">
-          <SunFill {...iconProps} />
+          <SunFill {...iconProps} color="white" />
         </div>
       </li>
     </ul>
-  )
-}
+  );
+};
 
-export default SocialLinks
+export default SocialLinks;
